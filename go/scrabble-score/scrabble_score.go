fed95cdf15845f74ef1d81378e5fb22ce282e96a
ric-v <ric-v@outlook.com>
2023-02-26 00:47:28 +0530

    added scrabble, difference of squares golang

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -1,5 +1,30 @@
 package scrabble
 
-func Score(word string) int {
-	panic("Please implement the Score function")
+import (
+	"fmt"
+	"strings"
+)
+
+func Score(word string) (output int) {
+
+	for _, letter := range strings.ToUpper(word) {
+		switch letter {
+		case 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T':
+			output += 1
+		case 'D', 'G':
+			output += 2
+		case 'B', 'C', 'M', 'P':
+			output += 3
+		case 'F', 'H', 'V', 'W', 'Y':
+			output += 4
+		case 'K':
+			output += 5
+		case 'J', 'X':
+			output += 8
+		case 'Q', 'Z':
+			output += 10
+		}
+	}
+	fmt.Println(output)
+	return
 }
