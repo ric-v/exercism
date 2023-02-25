9e4f08a21ffca7bd5736f74845c528083ed9c991
ric-v <ric-v@outlook.com>
2023-02-25 23:50:41 +0530

    added proverb solutino

--- go/proverb/proverb.go
+++ go/proverb/proverb.go
@@ -5,11 +5,18 @@
 // https://golang.org/doc/effective_go.html#commentary
 package proverb
 
+import "fmt"
+
 // Proverb should have a comment documenting it.
-func Proverb(rhyme []string) []string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return []string{}
+func Proverb(rhyme []string) (out []string) {
+
+	if len(rhyme) == 0 {
+		return
+	}
+
+	for i := 0; i < len(rhyme)-1; i++ {
+		out = append(out, fmt.Sprintf("For want of a %s the %s was lost.", rhyme[i], rhyme[i+1]))
+	}
+	out = append(out, fmt.Sprintf("And all for the want of a %s.", rhyme[0]))
+	return
 }
